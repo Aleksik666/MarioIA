542a3bf63ac8c0f688c0e01b0f1b36ef314bc20d
Aleksik666 <127124221+Aleksik666@users.noreply.github.com>
2025-03-11 11:34:50 +0100

    Update random_speedrun_play.lua

--- random_speedrun_play.lua
+++ random_speedrun_play.lua
@@ -8,65 +8,72 @@ ButtonNames = {
     "Right",
 }
 
-best_time = math.huge
-
 function getPositions()
     marioX = memory.readbyte(0x6D) * 0x100 + memory.readbyte(0x86)
     marioY = memory.readbyte(0x03B8) + 16
+
     screenX = memory.readbyte(0x03AD)
     screenY = memory.readbyte(0x03B8)
 end
 
-jump_weight = 0.8
-right_weight = 0.95
-left_weight = 0.05
-down_weight = 0.05
-turbo_weight = 0.8
-
-stuck_timer = 0
-max_stuck_time = 120
+jump_weight = 0.9
+base_left_weight = 0.15
+left_weight = 0.15
+right_weight = 0.9
+down_weight = 0.1
+run_weight = 1.0
 
 function randomInput()
     local outputs = {}
 
     if math.random() < jump_weight then
-        outputs["P1 A"] = math.random() < 0.5  -- Randomize jump height
+        outputs["P1 A"] = true
     end
-
+    
     if math.random() < right_weight then
         outputs["P1 Right"] = true
     end
-
+    
     if math.random() < left_weight then
         outputs["P1 Left"] = true
     end
-
-    if math.random() < turbo_weight then
-        outputs["P1 B"] = true
+    
+    outputs["P1 B"] = true
+    
+    if controller["P1 Left"] and controller["P1 Right"] then
+        controller["P1 Left"] = true
+        controller["P1 Right"] = false
     end
-
+    
+    if left_weight > base_left_weight then
+        left_weight = left_weight - 0.01
+    end
+    
     return outputs
 end
 
 function clearJoypad()
     controller = {}
-    for b = 1,#ButtonNames do
+    for b = 1, #ButtonNames do
         controller["P1 " .. ButtonNames[b]] = false
     end
     joypad.set(controller)
 end
 
 try = 1
+frame_count = 0
+best_time = math.huge
+stuck_frames = 0
 
 function doRun()
     savestate.load(Filename)
+    
     getPositions()
-
+    
     oldX = marioX
-    oldY = marioY
-    stuck_timer = 0
-    frame_count = 0
-
+    stuck_frames = 0
+    start_time = os.time()
+    
     if mainMoves ~= nil then    
         for a = 1, #mainMoves do
             clearJoypad()
@@ -79,44 +86,123 @@ function doRun()
     subMoves = {}
     
     while true do
+        if mainMoves == nil then
+            mainMoves = {}
+        end
+        
         getPositions()
+        
         clearJoypad()
+        
         controller = randomInput()
+        
         joypad.set(controller)
         table.insert(subMoves, controller)
-        
-        if memory.readbyte(0x000E) == 0x06 or memory.readbyte(0x000D) == 0x00 then  -- Detect death properly
-            break
-        end
-        
-        if oldX == marioX and oldY == marioY then
-            stuck_timer = stuck_timer + 1
+
+        if oldX == marioX then
+            stuck_frames = stuck_frames + 1
         else
-            stuck_timer = 0
+            stuck_frames = 0
         end
         
-        if stuck_timer > max_stuck_time then
-            jump_weight = 0.5  -- Allow bigger jumps
+        if stuck_frames > 300 then
+            left_weight = 0.6
             right_weight = 1.0
-            left_weight = 0.0
-            turbo_weight = 1.0
-            stuck_timer = 0
+            stuck_frames = 0
         end
         
         oldX = marioX
-        oldY = marioY
+        
         emu.frameadvance()
         frame_count = frame_count + 1
+
+        if memory.readbyte(0x000E) == 0x06 then
+            end_time = os.time()
+            current_time = end_time - start_time
+            
+            if current_time < best_time then
+                best_time = current_time
+                inputBytes = ""
+                for m = 1, #subMoves do
+                    local controller = subMoves[m]
+                    if controller["P1 Left"] then
+                        inputBytes = inputBytes .. 1
+                    else
+                        inputBytes = inputBytes .. 0
+                    end
+                    
+                    if controller["P1 Right"] then
+                        inputBytes = inputBytes .. 1
+                    else
+                        inputBytes = inputBytes .. 0
+                    end
+                    
+                    if controller["P1 A"] then
+                        inputBytes = inputBytes .. 1
+                    else
+                        inputBytes = inputBytes .. 0
+                    end
+                    
+                    if controller["P1 B"] then
+                        inputBytes = inputBytes .. 1
+                    else
+                        inputBytes = inputBytes .. 0
+                    end
+                end
+                
+                file = io.open("best_moves.txt", "w")
+                file:write(inputBytes)
+                file:close()
+            end
+            break
+        end
     end
     
-    if frame_count < best_time then
-        best_time = frame_count
-        mainMoves = subMoves
+    moveCount = #subMoves - 300
+    
+    if moveCount > 0 then
+        for m = 1, moveCount do
+            table.insert(mainMoves, subMoves[m])
+        end
     end
+    
+    inputBytes = ""
+    
+    for m = 1, #mainMoves do
+        local controller = mainMoves[m]
+        if controller["P1 Left"] then
+            inputBytes = inputBytes .. 1
+        else
+            inputBytes = inputBytes .. 0
+        end
+        
+        if controller["P1 Right"] then
+            inputBytes = inputBytes .. 1
+        else
+            inputBytes = inputBytes .. 0
+        end
+        
+        if controller["P1 A"] then
+            inputBytes = inputBytes .. 1
+        else
+            inputBytes = inputBytes .. 0
+        end
+        
+        if controller["P1 B"] then
+            inputBytes = inputBytes .. 1
+        else
+            inputBytes = inputBytes .. 0
+        end
+    end
+    
+    file = io.open(marioX .. "_" .. frame_count .. "_moves.txt", "w")
+    file:write(inputBytes)
     
     try = try + 1
+    
     doRun()
 end
 
 mainMoves = nil
+
 doRun()
